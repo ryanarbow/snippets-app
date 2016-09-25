8181da62440b9c69de008d18280565c1d5b8c85b
ryanarbow <ryanarbow@gmail.com>
2016-09-25 03:22:52 +0000

    ready to run

--- snippets.py
+++ snippets.py
@@ -34,7 +34,21 @@ def main():
     put_parser.add_argument("name", help="Name of the snippet")
     put_parser.add_argument("snippet", help="Snippet text")
     
+    get_parser = subparsers.add_parser("get", help="Retrieve a snippet")
+    put_parser.add_argument("name", help="Name of the snippet")
+    
     arguments = parser.parse_args()
+    #Convert parsed arguments from Namespace to dictionary
+    
+    arguments = vars(arguments)
+    command = arguments.pop("command")
+    
+    if command == "put":
+        name, snippet = put(**arguments)
+        print("Stored {!r} as {!r}".format(snippet, name))
+    elif command == "get":
+        snippet = get(**arguments)
+        print("Retrieved snippet: {!r}".format(snippet))
 
     
 if __name__== "__main__":
