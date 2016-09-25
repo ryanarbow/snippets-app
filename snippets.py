6d86e47f7766caaf3119ff7b65ea0048c85f05a3
ryanarbow <ryanarbow@gmail.com>
2016-09-25 00:33:46 +0000

    subparser put added

--- snippets.py
+++ snippets.py
@@ -25,7 +25,17 @@ def main():
     """Main function"""
     logging.info("Constructing parser")
     parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+   
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+    
+    #Subparser for the put command
+    logging.debug("Constructing the subparsers")
+    put_parser = subparsers.add_parser("put", help="Store a snippet")
+    put_parser.add_argument("name", help="Name of the snippet")
+    put_parser.add_argument("snippet", help="Snippet text")
+    
     arguments = parser.parse_args()
+
     
 if __name__== "__main__":
     main()
